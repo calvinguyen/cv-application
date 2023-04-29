02170d41226c511ac4d44d4d725cba6ac4729cc8
calvinguyen <77700030+calvinguyen@users.noreply.github.com>
2023-04-28 23:03:30 -0500

    Install and setup linter

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,3 +1,4 @@
+/* eslint-disable no-undef */
 module.exports = {
   env: { browser: true, es2020: true },
   extends: [
@@ -5,12 +6,19 @@ module.exports = {
     'plugin:react/recommended',
     'plugin:react/jsx-runtime',
     'plugin:react-hooks/recommended',
+    'airbnb',
+    'prettier',
   ],
   parserOptions: { ecmaVersion: 'latest', sourceType: 'module' },
   settings: { react: { version: '18.2' } },
-  plugins: ['react-refresh'],
+  plugins: ['react-refresh', 'prettier'],
   rules: {
     'react/prop-types': 0,
+    'react/react-in-jsx-scope': 0,
+    'react-hooks/exhaustive-deps': 0,
     'react-refresh/only-export-components': 'warn',
+    'no-console': 'off',
+    'no-unused-vars': 'warn',
+    'prettier/prettier': 'error',
   },
-}
+};
--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,10 @@
 function App() {
-
   return (
     <>
       <h1>CV Application</h1>
+      <p>content goes here</p>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
