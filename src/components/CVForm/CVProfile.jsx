5a5b2eda6caa4ce0eb736289fa05d69bec3f2c34
calvinguyen <77700030+calvinguyen@users.noreply.github.com>
2023-05-01 12:28:55 -0500

    Create and add styling to hobbies component

--- src/components/CVForm/CVProfile.jsx
+++ src/components/CVForm/CVProfile.jsx
@@ -69,6 +69,17 @@ const CVProfile = () => {
       
       <section className="hobbies">
         <h1 className="section-title section-title-profile">TECHNICAL SKILLS</h1>
+
+        <div className="hobbies-item-container">
+          <p className="hobbies-item">JavaScript</p>
+          <p className="hobbies-item">Python</p>
+          <p className="hobbies-item">Ruby</p>
+          <p className="hobbies-item">Scala</p>
+          <p className="hobbies-item">Go</p>
+          <p className="hobbies-item">HTML</p>
+          <p className="hobbies-item">CSS</p>
+          <p className="hobbies-item">Ansible</p>
+        </div>
       </section>
       
       <section className="education">
--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,6 +1,11 @@
 import CVForm from './CVForm/CVForm';
 import CVEditForm from './CVEditForm/CVEditForm';
 
+/* 
+  TODO: Hold Form State in Main
+  TODO: Pass State props to Form and Edit Form
+*/
+
 function Main() {
   return (
     <main>
