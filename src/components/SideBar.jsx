0c11565c4df5b4273f3d1a6a5903997830d329e7
calvinguyen <77700030+calvinguyen@users.noreply.github.com>
2023-05-03 17:12:46 -0500

    Implement navbar buttons and styling

--- src/components/SideBar.jsx
+++ src/components/SideBar.jsx
@@ -1,10 +1,73 @@
-import React from 'react';
 import '../assets/css/SideBar.css';
+import AccountCircleIcon from '@mui/icons-material/AccountCircle';
+import ShareIcon from '@mui/icons-material/Share';
+import TuneIcon from '@mui/icons-material/Tune';
+import SkateboardingIcon from '@mui/icons-material/Skateboarding';
+import SchoolIcon from '@mui/icons-material/School';
+import WorkHistoryIcon from '@mui/icons-material/WorkHistory';
+import FolderIcon from '@mui/icons-material/Folder';
+import Diversity1Icon from '@mui/icons-material/Diversity1';
+import WorkspacePremiumIcon from '@mui/icons-material/WorkspacePremium';
 
 const SideBar = () => {
   return (
     <nav className="sidebar">
-      <div>navbar</div>
+      <ul>
+        <li>
+          <a href="#profile-form">
+            <AccountCircleIcon />
+            <p>Profile</p>
+          </a>
+        </li>
+        <li>
+          <a href="#social-form">
+            <ShareIcon />
+            <p>Social</p>
+          </a>
+        </li>
+        <li>
+          <a href="#skills-form">
+            <TuneIcon />
+            <p>Skills</p>
+          </a>
+        </li>
+        <li>
+          <a href="#hobbies-form">
+            <SkateboardingIcon />
+            <p>Hobby</p>
+          </a>
+        </li>
+        <li>
+          <a href="#school-form">
+            <SchoolIcon />
+            <p>School</p>
+          </a>
+        </li>
+        <li>
+          <a href="#work-form">
+            <WorkHistoryIcon />
+            <p>Work</p>
+          </a>
+        </li>
+        <li>
+          <a href="#projects-form">
+            <FolderIcon />
+            <p>Projects</p>
+          </a>
+        </li>
+        <li>
+          <a href="#orgs-form">
+            <Diversity1Icon />
+            <p>Orgs...</p>
+          </a>
+        </li>
+        <li>
+          <a href="#certs-form">
+            <WorkspacePremiumIcon />
+            <p>Cert...</p>
+          </a>
+        </li>
+      </ul>
     </nav>
   );
 };
